1ffc972a00ab50f1c3b21d1ec02b7189a7a9f12c
preciz <barna.kovacs.codes@gmail.com>
2019-09-26 15:57:49 +0200

    Test Abit.intersect

--- test/abit_test.exs
+++ test/abit_test.exs
@@ -27,4 +27,31 @@ defmodule AbitTest do
     assert 321 = :atomics.get(merged_ref, 1)
     assert 123 = :atomics.get(merged_ref, 2)
   end
+
+  test "intersect of 2 atomics bit arrays returns left reference" do
+    ref_a = :atomics.new(2, signed: false)
+    ref_b = :atomics.new(2, signed: false)
+
+    ref_a |> :atomics.put(1, 321)
+    ref_a |> :atomics.put(2, 1)
+    ref_b |> :atomics.put(2, 122)
+
+    intersect_ref = Abit.intersect(ref_a, ref_b)
+
+    assert intersect_ref == ref_a
+  end
+
+  test "intersect of 2 atomics bit arrays intersect values" do
+    ref_a = :atomics.new(2, signed: false)
+    ref_b = :atomics.new(2, signed: false)
+
+    ref_a |> :atomics.put(1, 321)
+    ref_a |> :atomics.put(2, 1)
+    ref_b |> :atomics.put(2, 123)
+
+    intersect_ref = Abit.intersect(ref_a, ref_b)
+
+    assert 0 = :atomics.get(intersect_ref, 1)
+    assert 1 = :atomics.get(intersect_ref, 2)
+  end
 end
