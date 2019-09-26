8909941809ae5541ffece93edb8671517e8444c4
preciz <barna.kovacs.codes@gmail.com>
2019-09-26 16:15:35 +0200

    Prepare mix.exs for first release

--- mix.exs
+++ mix.exs
@@ -1,13 +1,22 @@
 defmodule Abit.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @github "https://github.com/preciz/abit"
+
   def project do
     [
       app: :abit,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package(),
+      homepage_url: @github,
+      description: """
+      Helper functions to use :atomics as a bit array in Elixir.
+      """
     ]
   end
 
@@ -18,6 +27,24 @@ defmodule Abit.MixProject do
   end
 
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+    ]
+  end
+
+  defp docs do
+    [
+      source_ref: "v#{@version}",
+      source_url: @github,
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Barna Kovacs"],
+      licenses: ["MIT"],
+      links: %{github: @github},
+      files: ~w(LICENSE.md README.md mix.exs)
+    ]
   end
 end
