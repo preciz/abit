7d696e386560ddc1c1bcf0b525b0b25e485feb40
preciz <barna.kovacs@tutanota.com>
2024-07-20 19:34:12 +0200

    Implement Atomics.serialize/1 and Atomics.deserialize/1

--- lib/abit/atomics.ex
+++ lib/abit/atomics.ex
@@ -1,9 +1,6 @@
 defmodule Abit.Atomics do
   @moduledoc """
-  Internal module for working with Erlang's :atomics.
-
   This module provides utility functions for working with Erlang's :atomics.
-  It should not be used directly by users of the Abit library.
   """
 
   @doc """
@@ -97,5 +94,100 @@ defmodule Abit.Atomics do
       index - 1,
       :atomics.get(atomics_ref, index) == int
     )
+  end
+
+  @doc """
+  Serializes an :atomics reference into a binary.
+
+  This function takes an :atomics reference and returns a binary where each
+  64-bit integer in the :atomics array is encoded in big-endian format.
+
+  ## Parameters
+
+    * `atomics_ref` - A reference to an :atomics array.
+
+  ## Returns
+
+  A binary containing the serialized :atomics data.
+
+  ## Examples
+
+      iex> ref = :atomics.new(2, signed: false)
+      iex> :atomics.put(ref, 1, 1)
+      iex> :atomics.put(ref, 2, 2)
+      iex> Abit.Atomics.serialize(ref)
+      <<0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2>>
+
+  """
+  @spec serialize(reference()) :: binary()
+  def serialize(atomics_ref) when is_reference(atomics_ref) do
+    %{size: size} = :atomics.info(atomics_ref)
+
+    signature_byte(atomics_ref) <> do_serialize(atomics_ref, 1, size, <<>>)
+  end
+
+  defp signature_byte(atomics_ref) when is_reference(atomics_ref) do
+    case :atomics.info(atomics_ref) do
+      %{min: 0} -> <<0>>
+      %{min: n} when n < 0 -> <<1>>
+    end
+  end
+
+  defp do_serialize(_atomics_ref, index, size, acc) when index > size, do: acc
+
+  defp do_serialize(atomics_ref, index, size, acc) do
+    value = :atomics.get(atomics_ref, index)
+    do_serialize(atomics_ref, index + 1, size, acc <> <<value::64-big>>)
+  end
+
+  @doc """
+  Deserializes a binary into an :atomics reference.
+
+  This function takes a binary that was previously serialized using `serialize/1`
+  and reconstructs an :atomics reference from it.
+
+  ## Parameters
+
+    * `binary` - A binary containing the serialized :atomics data.
+
+  ## Returns
+
+  A reference to a new :atomics array containing the deserialized data.
+
+  ## Examples
+
+      iex> ref = :atomics.new(2, [])
+      iex> :atomics.put(ref, 1, 10)
+      iex> :atomics.put(ref, 2, -20)
+      iex> serialized = Abit.Atomics.serialize(ref)
+      iex> deserialized_ref = Abit.Atomics.deserialize(serialized)
+      iex> Abit.Atomics.to_list(deserialized_ref)
+      [10, -20]
+
+  """
+  @spec deserialize(binary()) :: reference()
+  def deserialize(<<signature_byte::8, rest::binary>>) do
+    signed? =
+      case signature_byte do
+        0 -> false
+        1 -> true
+      end
+
+    size = byte_size(rest) |> div(8)
+    atomics_ref = :atomics.new(size, signed: signed?)
+    do_deserialize(signed?, atomics_ref, rest, 1)
+    atomics_ref
+  end
+
+  defp do_deserialize(_signed, _atomics_ref, <<>>, _index), do: :ok
+
+  defp do_deserialize(false, atomics_ref, <<value::64-big, rest::binary>>, index) do
+    :atomics.put(atomics_ref, index, value)
+    do_deserialize(false, atomics_ref, rest, index + 1)
+  end
+
+  defp do_deserialize(true, atomics_ref, <<value::64-big-signed, rest::binary>>, index) do
+    :atomics.put(atomics_ref, index, value)
+    do_deserialize(true, atomics_ref, rest, index + 1)
   end
 end
--- test/abit/atomics_test.exs
+++ test/abit/atomics_test.exs
@@ -52,4 +52,106 @@ defmodule Abit.AtomicsTest do
       refute Atomics.member?(ref, 18_446_744_073_709_551_616)
     end
   end
+
+  describe "serialize/1" do
+    test "serializes unsigned atomics" do
+      ref = :atomics.new(3, signed: false)
+      :atomics.put(ref, 1, 10)
+      :atomics.put(ref, 2, 20)
+      :atomics.put(ref, 3, 30)
+
+      serialized = Atomics.serialize(ref)
+
+      assert <<0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 0, 30>> =
+               serialized
+    end
+
+    test "serializes signed atomics" do
+      ref = :atomics.new(3, signed: true)
+      :atomics.put(ref, 1, -10)
+      :atomics.put(ref, 2, 20)
+      :atomics.put(ref, 3, -30)
+
+      serialized = Atomics.serialize(ref)
+
+      assert <<1, 255, 255, 255, 255, 255, 255, 255, 246, 0, 0, 0, 0, 0, 0, 0, 20, 255, 255, 255,
+               255, 255, 255, 255, 226>> = serialized
+    end
+  end
+
+  describe "deserialize/1" do
+    test "deserializes unsigned atomics" do
+      original_ref = :atomics.new(3, signed: false)
+      :atomics.put(original_ref, 1, 10)
+      :atomics.put(original_ref, 2, 20)
+      :atomics.put(original_ref, 3, 30)
+
+      serialized = Atomics.serialize(original_ref)
+      deserialized_ref = Atomics.deserialize(serialized)
+
+      assert Atomics.to_list(deserialized_ref) == [10, 20, 30]
+      assert :atomics.info(deserialized_ref).min == 0
+    end
+
+    test "deserializes signed atomics" do
+      original_ref = :atomics.new(3, signed: true)
+      :atomics.put(original_ref, 1, -10)
+      :atomics.put(original_ref, 2, 20)
+      :atomics.put(original_ref, 3, -30)
+
+      serialized = Atomics.serialize(original_ref)
+      deserialized_ref = Atomics.deserialize(serialized)
+
+      assert Atomics.to_list(deserialized_ref) == [-10, 20, -30]
+      assert :atomics.info(deserialized_ref).min < 0
+    end
+
+    test "roundtrip serialization and deserialization for signed atomics" do
+      original_ref = :atomics.new(5, signed: true)
+      :atomics.put(original_ref, 1, -1_000_000)
+      :atomics.put(original_ref, 2, 0)
+      :atomics.put(original_ref, 3, 1_000_000)
+      # Min 64-bit signed int
+      :atomics.put(original_ref, 4, -9_223_372_036_854_775_808)
+      # Max 64-bit signed int
+      :atomics.put(original_ref, 5, 9_223_372_036_854_775_807)
+
+      serialized = Atomics.serialize(original_ref)
+      deserialized_ref = Atomics.deserialize(serialized)
+
+      assert Atomics.to_list(deserialized_ref) == [
+               -1_000_000,
+               0,
+               1_000_000,
+               -9_223_372_036_854_775_808,
+               9_223_372_036_854_775_807
+             ]
+
+      assert :atomics.info(deserialized_ref).size == 5
+      assert :atomics.info(deserialized_ref).min < 0
+    end
+
+    test "roundtrip serialization and deserialization for unsigned atomics" do
+      original_ref = :atomics.new(5, signed: false)
+      :atomics.put(original_ref, 1, 1_000_000)
+      :atomics.put(original_ref, 2, 0)
+      :atomics.put(original_ref, 3, 2_000_000)
+      # Max 64-bit unsigned int
+      :atomics.put(original_ref, 4, 18_446_744_073_709_551_615)
+
+      serialized = Atomics.serialize(original_ref)
+      deserialized_ref = Atomics.deserialize(serialized)
+
+      assert Atomics.to_list(deserialized_ref) == [
+               1_000_000,
+               0,
+               2_000_000,
+               18_446_744_073_709_551_615,
+               0
+             ]
+
+      assert :atomics.info(deserialized_ref).size == 5
+      assert :atomics.info(deserialized_ref).min == 0
+    end
+  end
 end
